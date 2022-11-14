4ab0facabc950a1e71dc514dd58d811c13c42641
piyush6716721 <113968283+piyush6716721@users.noreply.github.com>
2022-11-14 13:03:13 +0530

    hello

---
+++ index.js
@@ -0,0 +1,6 @@
+let signinbtn=document.querySelector('button');
+signinbtn.addEventListener('click' , showmsg);
+
+function showmsg(){
+    alert(" Dear sumit you are not eligible to sign in  sorrry🙏")
+}
